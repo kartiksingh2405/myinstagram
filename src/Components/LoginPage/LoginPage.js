e1841cb2ea75e2f53374a6c7ea50a5685917629e
kartiksingh2405 <kartiksingh2405@gmail.com>
2023-06-17 11:39:56 +0530

    add image

--- src/Components/LoginPage/LoginPage.js
+++ src/Components/LoginPage/LoginPage.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react';
 import './LoginPage.css';
 import Grid from '@mui/material/Grid';
+import inst_image from '../../images/9364675fb26a.svg';
+import insta_logo from '../../images/logoinsta.png';
+
 
 const LoginPage = () => {
   return (
     <div>
       <Grid container>
         <Grid item xs={3}>
-          jhjnss
+          jhjns
         </Grid>
         <Grid item xs={6}>
-          jhbj
+          <div>
+            <div>
+              <img src={inst_image} alt="" width="454px" />
+            </div>
+            <div>
+              <img src={insta_logo} alt="" />
+            </div>
+          </div>
         </Grid>
         <Grid item xs={3}>
           jkjl
