b7444f6cfedfcd1e5fd5285dbaa030c910216d58
kartiksingh2405 <kartiksingh2405@gmail.com>
2023-06-17 10:36:16 +0530

    install materail ui

---
+++ src/Components/LoginPage/LoginPage.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './LoginPage.css'
+// import Grid from '@material-ui/core/Grid';
+
+const LoginPage = () => {
+  return (
+    <div>
+      {/* <Grid container>
+        <Grid item xs={3}>
+          jhjns
+        </Grid>
+        <Grid item xs={6}>
+          jhbj
+        </Grid>
+        <Grid item xs={3}>
+          jkjl
+        </Grid>
+      </Grid> */}
+    </div>
+  );
+};
+
+export default LoginPage;
