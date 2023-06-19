fa095a4c125c08750dfb19e18fb382698074c4b5
kartiksingh2405 <kartiksingh2405@gmail.com>
2023-06-19 18:08:55 +0530

    creating Home Page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import HomePage from './Components/HomePage/Home';
 import LoginPage from './Components/LoginPage/LoginPage';
 
 function App() {
   return (
     <div className="App">
-      <LoginPage />
+      {/*<LoginPage />*/ }
+      <HomePage/>
     </div>
   );
 }
