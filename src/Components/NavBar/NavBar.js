364b615d9fa0cf00f1668a4f8e4c4d04e6284fcc
kartiksingh2405 <kartiksingh2405@gmail.com>
2023-06-19 18:10:16 +0530

    creating navbar

---
+++ src/Components/NavBar/NavBar.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class NavBar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  };
+    }
+    render() {
+        return (
+            <div>Nav Bar</div>
+        );
+    }
+}
+
+export default NavBar;
