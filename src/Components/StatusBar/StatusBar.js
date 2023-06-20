018e90092869ced8904d44f6f510114e570ca3b8
kartiksingh2405 <kartiksingh2405@gmail.com>
2023-06-20 17:03:26 +0530

    updating status bar

--- src/Components/StatusBar/StatusBar.js
+++ src/Components/StatusBar/StatusBar.js
@@ -5,52 +5,52 @@ import statusimg from "../../images/pp1.png";
 class StatusBar extends Component {
     constructor(props) {
         super(props);
-        this.state = {  };
+        this.state = { 
+            statusList: []
+         }
+    }
+
+    componentDidMount(){
+        this.getData();
+    }
+
+    getData=()=>{
+         let data=[
+            {
+                 "username":"kartik",
+                 "imageURL":"../../images/pp1.png"
+            },
+            {
+               "username":"abcs",
+                "imageURL":"../../images/pp1.png"
+            },
+            {
+                "username":"abcs",
+                 "imageURL":"../../images/pp1.png"
+             },
+             {
+                "username":"abcs",
+                 "imageURL":"../../images/pp1.png"
+             },
+             {
+                "username":"abcs",
+                 "imageURL":"../../images/pp1.png"
+             },
+        ]
+        this.setState({statusList: data})
     }
     render() {
         return (
             <div>
                 <div className="statusbar__container">
-                    <div className="status">
-                        <Avatar className="statusbar__status" src={statusimg} />
-                        <div className="statusbar__text">Kartik</div>
-                    </div>
-                    <div className="status">
-                        <Avatar className="statusbar__status" src={statusimg} />
-                        <div className="statusbar__text">Kartik</div>
-                    </div>
-                    <div className="status">
-                        <Avatar className="statusbar__status" src={statusimg} />
-                        <div className="statusbar__text">Kartik</div>
-                    </div>
-                    <div className="status">
-                        <Avatar className="statusbar__status" src={statusimg} />
-                        <div className="statusbar__text">Kartik</div>
-                    </div>
-                    <div className="status">
-                        <Avatar className="statusbar__status" src={statusimg} />
-                        <div className="statusbar__text">Kartik</div>
-                    </div>
-                    <div className="status">
-                        <Avatar className="statusbar__status" src={statusimg} />
-                        <div className="statusbar__text">Kartik</div>
-                    </div>
-                    <div className="status">
-                        <Avatar className="statusbar__status" src={statusimg} />
-                        <div className="statusbar__text">Kartik</div>
-                    </div>
-                    <div className="status">
-                        <Avatar className="statusbar__status" src={statusimg} />
-                        <div className="statusbar__text">Kartik</div>
-                    </div>
-                    <div className="status">
-                        <Avatar className="statusbar__status" src={statusimg} />
-                        <div className="statusbar__text">Kartik</div>
-                    </div>
-                    <div className="status">
-                        <Avatar className="statusbar__status" src={statusimg} />
-                        <div className="statusbar__text">Kartik</div>
-                    </div>
+                    {
+                        this.state.statusList.map((item,index)=>(
+                            <div className="status">
+                                <Avatar className="statusbar__status" src={item.path} />
+                                <div className="statusbar__text">{item.userName}</div>
+                            </div>
+                        ))
+                    }
                 </div>
             </div>
         );
