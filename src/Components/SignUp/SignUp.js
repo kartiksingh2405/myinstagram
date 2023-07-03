f40cc1e3c0c1c3b97d7156c55cf316aa399e740b
kartiksingh2405 <kartiksingh2405@gmail.com>
2023-07-03 17:33:25 +0530

    SignUp update

--- src/Components/SignUp/SignUp.js
+++ src/Components/SignUp/SignUp.js
@@ -1,20 +1,40 @@
 import React, { Component } from 'react'
 import './SignUp.css'
-
+import {storage,auth} from "./firebase";
 
 class SignUp extends Component {
     constructor(props) {
         super(props);
-        this.state = {  };
+        this.state = { 
+            emailId : null,
+            name : null,
+            userName : null,
+            password : null 
+         }
     }
+
+    newSignUp=()=>{
+        auth.createUserWithEmailAndPassword(this.state.emailId, this.state.password)
+        .then((userCredential) => {
+            // Signed in 
+            var user = userCredential.user;
+            // ...
+        })
+        .catch((error) => {
+            var errorCode = error.code;
+            var errorMessage = error.message;
+
+        });
+    }
+    
     render() {
         return (
             <div>
-                <input className="loginpage__text" type="text" placeholder="Mobile Number or Email"/>
-                <input className="loginpage__text" type="text" placeholder="Full Name"/>
-                <input className="loginpage__text" type="text" placeholder="Username"/>
-                <input className="loginpage__text" type="password" placeholder="Password"/>
-                <button className="login__button" >Sign Up</button>
+                <input className="loginpage__text" onChange={(event)=>{this.state.emailId=event.currentTarget.value;}} type="text" placeholder="Mobile Number or Email"   / >
+                <input className="loginpage__text" onChange={(event)=>{this.state.name=event.currentTarget.value;}} type="text" placeholder="Full Name"/>
+                <input className="loginpage__text" onChange={(event)=>{this.state.userName=event.currentTarget.value;}} type="text" placeholder="Username"/>
+                <input className="loginpage__text" onChange={(event)=>{this.state.password=event.currentTarget.value;}} type="password" placeholder="Password"/>
+                <button className="login__button" onClick={this.newSignUp} >Sign Up</button>
             </div>
         );
     }
