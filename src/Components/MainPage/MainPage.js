59d852cd56c4b0d69803265e802cd61b210be594
kartiksingh2405 <kartiksingh2405@gmail.com>
2023-07-10 17:07:22 +0530

    update mainpage

--- src/Components/MainPage/MainPage.js
+++ src/Components/MainPage/MainPage.js
@@ -8,7 +8,8 @@ class MainPage extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            postArray:[] 
+            postArray:[], 
+            progressBar: "",
         };
     }
     
@@ -47,6 +48,7 @@ class MainPage extends Component {
 
 upload = (event) => {
   let image = event.target.files[0];
+  const thisContext=this;
   if (image == null || image == undefined)
     return;
     const storage = getStorage();
@@ -56,14 +58,41 @@ upload = (event) => {
   uploadTask.on(
     "state_changed",
     function (snapshot) {
-      // Progress updates can be handled here
+      var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      thisContext.setState({progressBar: progress});
     },
     function (error) {
       // Error handling can be done here
     },
     function () {
       getDownloadURL(uploadTask.snapshot.ref).then(function (downloadURL) {
-        // Handle the download URL here
+        console.log(downloadURL); 
+
+        let payload = {
+
+          "postId": Math.floor(Math.random()*100000).toString(),
+          "userId": localStorage.getItem("users").uid,
+          "postPath": downloadURL,
+          "timestamp": new Date().getTime(),
+          "likeCount": "0"
+
+        }
+
+        const requestOptions ={
+          method: "POST",
+          headers: { 'Content-Type': 'application/json' },
+          body : JSON.stringify(payload),
+        }
+
+        fetch("http://localhost:8080/post",requestOptions)
+        .then(response => response.json())
+        .then(data => {
+          console.log(data);
+        })
+        .catch(error =>{
+
+        })
+
       });
     }
   );
@@ -82,6 +111,7 @@ upload = (event) => {
                         <input onChange={this.upload} id="file-upload" type="file"/>
                     </div>
                     <div className="mainpage__divider"></div>  
+                    <div className="upload_text">{this.state.progressBar}</div>
                 </div>
                 {
                      this.state.postArray.map((item,index)=>(
