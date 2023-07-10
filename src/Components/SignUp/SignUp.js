dda6cee4186291b2de356925143513dd6d5aeffc
kartiksingh2405 <kartiksingh2405@gmail.com>
2023-07-10 20:13:48 +0530

    update sign up , main page

--- src/Components/SignUp/SignUp.js
+++ src/Components/SignUp/SignUp.js
@@ -1,69 +1,70 @@
 import React, { Component } from 'react';
 import "./SignUp.css";
-import {storage,auth} from "../firebase";
+import { storage, auth } from "../firebase";
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
 class SignUp extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { 
-            emailId : null,
-            name: null,
-            userName: null,
-            password: null
-         }
-    }
-    newSignUp=()=>{
-        const auth = getAuth();
-        createUserWithEmailAndPassword(auth,this.state.emailId, this.state.password)
-        .then((userCredential) => {
-            // Signed in 
-            var user = userCredential.user;
-            
-            let payload = {
-                "userId": user.uid,
-                "userName": this.state.userName,
-                "name": this.state.name,
-                "profileImage": ""
-            }
+  constructor(props) {
+    super(props);
+    this.state = {
+      emailId: null,
+      name: null,
+      userName: null,
+      password: null
+    };
+  }
 
-            const requestOptions ={
-                method: "POST",
-                headers: { 'Content-Type': 'application/json' },
-                body : JSON.stringify(payload),
-            }
+  newSignUp = () => {
+    const auth = getAuth();
+    createUserWithEmailAndPassword(auth, this.state.emailId, this.state.password)
+      .then((userCredential) => {
+        // Signed in
+        var user = userCredential.user;
 
-            fetch("http://localhost:8080/users",requestOptions)
-            .then(response => response.json())
-            .then(data => {
-                localStorage.setItem("users", user);
-                window.location.reload();
-            })
-            .catch(error =>{
+        let payload = {
+          "userId": user.uid,
+          "userName": this.state.userName,
+          "name": this.state.name,
+          "profileImage": ""
+        };
 
-            })
+        const requestOptions = {
+          method: "POST",
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify(payload),
+        };
 
-            // ...
-        })
-        .catch((error) => {
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            // ..
-        });
-    }
+        fetch("http://localhost:8080/users", requestOptions)
+          .then(response => response.json())
+          .then(data => {
+            localStorage.setItem("users", JSON.stringify(user));
+            window.location.reload();
+          })
+          .catch(error => {
 
-    render() { 
-        return ( 
-            <div>
-                <input className="loginpage__text" onChange={(event)=>{this.state.emailId=event.currentTarget.value;}} type="text" placeholder="Mobile Number or Email"   / >
-                <input className="loginpage__text" onChange={(event)=>{this.state.name=event.currentTarget.value;}} type="text" placeholder="Full Name"/>
-                <input className="loginpage__text" onChange={(event)=>{this.state.userName=event.currentTarget.value;}} type="text" placeholder="Username"/>
-                <input className="loginpage__text" onChange={(event)=>{this.state.password=event.currentTarget.value;}} type="password" placeholder="Password"/>
+          });
 
-                <button className="login__button" onClick={this.newSignUp} >Sign up</button>
-            </div>
-         );
-    }
+        // ...
+      })
+      .catch((error) => {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // ..
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        <input className="loginpage__text" onChange={(event) => { this.setState({ emailId: event.currentTarget.value }); }} type="text" placeholder="Mobile Number or Email" />
+        <input className="loginpage__text" onChange={(event) => { this.setState({ name: event.currentTarget.value }); }} type="text" placeholder="Full Name" />
+        <input className="loginpage__text" onChange={(event) => { this.setState({ userName: event.currentTarget.value }); }} type="text" placeholder="Username" />
+        <input className="loginpage__text" onChange={(event) => { this.setState({ password: event.currentTarget.value }); }} type="password" placeholder="Password" />
+
+        <button className="login__button" onClick={this.newSignUp}>Sign up</button>
+      </div>
+    );
+  }
 }
- 
+
 export default SignUp;
