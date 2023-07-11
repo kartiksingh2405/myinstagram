2074eef057a0e526529152200428f81ac8dc82cd
kartiksingh2405 <kartiksingh2405@gmail.com>
2023-07-11 16:51:03 +0530

    status update

--- src/Components/StatusBar/StatusBar.js
+++ src/Components/StatusBar/StatusBar.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react'
 import "./StatusBar.css";
 import Avatar from '@mui/material/Avatar';
 import statusimg from "../../images/pp1.png";
-import uploadImage from "../../images/upload.png";
- 
+import uploadimage from "../../images/statusadd.png";
+import { getStorage, ref, uploadBytesResumable, getDownloadURL, uploadTask } from 'firebase/storage'; 
 class StatusBar extends Component {
     constructor(props) {
         super(props);
@@ -17,46 +17,115 @@ class StatusBar extends Component {
     }
 
     getData=()=>{
-         let data=[
-            {
-                 "username":"kartik",
-                 "imageURL":"../../images/pp1.png"
-            },
-            {
-               "username":"abcs",
-                "imageURL":"../../images/pp1.png"
-            },
-            {
-                "username":"abcs",
-                 "imageURL":"../../images/pp1.png"
-             },
-             {
-                "username":"abcs",
-                 "imageURL":"../../images/pp1.png"
-             },
-             {
-                "username":"abcs",
-                 "imageURL":"../../images/pp1.png"
-             },
-        ]
-        this.setState({statusList: data})
-    }
-    render() {
-        return (
-            <div>
-                <div className="statusbar__container">
-                <img className="statusbar__upload" src={uploadImage} alt="" width="55px" height="55px"/>
-                    {
-                        this.state.statusList.map((item,index)=>(
-                            <div className="status">
-                                <Avatar className="statusbar__status" src={statusimg} />
-                                <div className="statusbar__text">{item.userName}</div>
-                            </div>
-                        ))
-                    }
-                </div>
-            </div>
+        // let data=[
+        //     {
+        //         "username":"anindya_bunny",
+        //         "imageURL":"https://darresne.com/img/female-avatar.png"
+        //      },
+        //      {
+        //         "username":"abcs",
+        //         "imageURL":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJYxr247w5ckIok4oLED58Lm7koT7pj4225A&usqp=CAU"
+        //      },
+        //      {
+        //         "username":"qwe",
+        //         "imageURL":"https://www.w3schools.com/w3css/img_avatar3.png"
+        //      },
+        //      {
+        //         "username":"jyjj",
+        //         "imageURL":"https://darresne.com/img/female-avatar.png"
+        //      },
+        //      {
+        //         "username":"jyjj",
+        //         "imageURL":"https://www.w3schools.com/w3css/img_avatar3.png"
+        //      },
+        //      {
+        //         "username":"jyjj",
+        //         "imageURL":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGonDgYzVXUcaKSWbvyH_ICVD23aI4zlRMJQ&usqp=CAU"
+        //      },
+        //      {
+        //         "username":"jyjj",
+        //         "imageURL":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJYxr247w5ckIok4oLED58Lm7koT7pj4225A&usqp=CAU"
+        //      },
+        //      {
+        //         "username":"jyjj",
+        //         "imageURL":"../../images/pp1.png"
+        //      }
+        // ]
+
+        fetch('http://localhost:8080/status')
+            .then(response => response.json())
+            .then(data => {
+                this.setState({statusList: data});
+        });
+    }   
+
+    uploadStatus =(event)=>{
+        let image=event.target.files[0];
+        const thisContext=this;
+        if(image == null || image == undefined)
+            return;
+
+            const storage = getStorage();
+            var storageRef = ref(storage, "images/" + image.name);
+            var uploadTask = uploadBytesResumable(storageRef, image);
+        uploadTask.on(
+          "state_changed",
+          function (snapshot) {
+          },
+          function (error) {
+          },
+          function () {
+            getDownloadURL(uploadTask.snapshot.ref).then(function (downloadURL) {
+                console.log(downloadURL);
+
+                let payload = {
+                    "statusId": Math.floor(Math.random()*100000).toString(),
+                    "userId": JSON.parse(localStorage.getItem("users")).uid,
+                    "path": downloadURL,
+                    "timestamp": new Date().getTime()
+                }
+    
+                const requestOptions ={
+                    method: "POST",
+                    headers: { 'Content-Type': 'application/json' },
+                    body : JSON.stringify(payload),
+                }
+    
+                fetch("http://localhost:8080/status",requestOptions)
+                .then(response => response.json())
+                .then(data => {
+                    thisContext.getData();
+                })
+                .catch(error =>{
+    
+                })
+                
+            })
+            }
         );
+    }
+
+    render() { 
+        return ( 
+        <div>
+            <div className="statusbar__container">
+            <div className="fileupload">
+                <label for="file-upload-status" >
+                    <img className="statusbar__upload" src={uploadimage} width="55px" height="55px" alt=""/>
+                </label>
+                    <input id="file-upload-status" onChange={this.uploadStatus} type="file"/>
+            </div>
+                {
+                    this.state.statusList.map((item,index)=>(
+                        <div className="status">
+                            <Avatar className="statusbar__status" src={item.path} />
+                            <div className="statusbar__text">{item.userName}</div>
+                        </div>
+                    ))
+                }
+            </div>
+        </div>
+         );
     }
 }
 
